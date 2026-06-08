88047dfe1439deac25576661fc7af401190d7118
KlanfYun <gareforza2020@outlook.es>
2026-06-07 20:31:07 -0600

    1.2.1 Compresion de imagenes

--- jsjs.js
+++ jsjs.js
@@ -36,28 +36,28 @@
         document.getElementById('navbar-catalogo').innerHTML = nav_catalogo;
       });
 
-      //**BACKGROUND PARA CADA CATEGORIA------------------------------------------------------------------------------------------------------------------------------------------------------ */
-      if(document.title != 'Dire Wolf | Colecciones' && 
-        document.title != 'Dire Wolf | Contacto' && 
-        document.title != 'Dire Wolf | Inicio'){
-        back_img = {
-          Florales: '../img/zbackback/back-flor.jpg',
-          Calakas:  '../img/zbackback/back_calaka.jpg',
-          Artistas: '../img/zbackback/back_artistas.jpg',
-          Anti:     '../img/zbackback/back_anti.jpg',
-          Motos:    '../img/zbackback/back_motos.jpg',
-          Carros:   '../img/zbackback/back_carros.jpg',
-          Urbano:   '../img/zbackback/back_urbano.jpg',
-          Prendas:  'img/prendasechas.jpg'
-        }
-        
-        document.addEventListener('DOMContentLoaded', () => {
-            titulo = (document.title).split(" ");//Covertir en lista o vector
-            cual = titulo[3];//luego selecionar el nombre de la categoria 
-            document.body.style.backgroundImage = `url('${back_img[cual]}')`;
-            document.body.style.backgroundSize = 'cover';
-        });
-      }
+      //**BACKGROUND PARA CADA CATEGORIA-------------------------------------
+//*     if(document.title != 'Dire Wolf | Colecciones' && 
+//*        document.title != 'Dire Wolf | Contacto' && 
+//*        document.title != 'Dire Wolf | Inicio'){
+//*        back_img = {
+//*          Florales: '../img/zbackback/back-flor.jpg',
+//*          Calakas:  '../img/zbackback/back_calaka.jpg',
+//*          Artistas: '../img/zbackback/back_artistas.jpg',
+//*          Anti:     '../img/zbackback/back_anti.jpg',
+//*          Motos:    '../img/zbackback/back_motos.jpg',
+//*          Carros:   '../img/zbackback/back_carros.jpg',
+//*          Urbano:   '../img/zbackback/back_urbano.jpg',
+//*          Prendas:  'img/prendasechas.jpg'
+//*        }
+//*        
+//*        document.addEventListener('DOMContentLoaded', () => {
+//*            titulo = (document.title).split(" ");//Covertir en lista o vector
+//*            cual = titulo[3];//luego selecionar el nombre de la categoria 
+//*            document.body.style.backgroundImage = `url('${back_img[cual]}')`;
+//*            document.body.style.backgroundSize = 'cover';
+//*        });
+//*      }
 
       //**MARCAR EL NAV EN DONDE ESTE------------------------------------------------------------------------------------------------------------------------------------------------------ */
       document.addEventListener('DOMContentLoaded', () =>{
